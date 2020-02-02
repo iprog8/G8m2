e2963fbd4a4822b1990c6ba521f63da09a1941c4
BogdanG93 <bogdan_geagu93@yahoo.com>
2020-02-02 10:10:30 +0200

    tema 4

---
+++ Teme/Bogdan/L4/app/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    var results = $.getJSON("cities.json", function(data) {
+        for(var i = 0; i < data.length; i++) {
+            $("#cities").append('<option value="' + data[i].name + '">' + data[i].name + '</option');
+        }
+    });
