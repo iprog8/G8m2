0950a2641f498a65565cc9f48c3190793d21d9b2
Letitia Provian - M19881 <letitia.provian@microchip.com>
2020-02-06 21:20:16 +0200

    toate cele 9 commit-uri

---
+++ L5/js/app.js
@@ -0,0 +1,23 @@
+function clickme() {
+    alert('ai dat click');
+}
+
+function blurme(){
+    alert('am fost parasit');
+}
+
+function changeme(){
+    alert('a schimbat textul')
+}
+
+function focusme(){
+    alert('am fost focusat')
+}
+
+function outme(){
+    alert('am fost parasit 2')
+}
+
+function overme(){
+    alert('am fost atacat')
+}
