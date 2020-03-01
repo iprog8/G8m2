5c47167cef01a715dd738482e299a7e4a7122f9b
BogdanG93 <bogdan_geagu93@yahoo.com>
2020-03-01 21:00:40 +0200

    L12

---
+++ Teme/Bogdan/L12/js/app3.js
@@ -0,0 +1,20 @@
+var imagePlace;
+
+function Aprins() {
+    imagePlace = document.getElementById('Light-Bulb');
+
+    if (imagePlace != null) {
+        imagePlace.src = "images/bulb-on.png";
+        imagePlace.alt = "Becul este aprins";
+        imagePlace.title = "Becul este aprins";
+        imagePlace == null;
+    }
+    else {
+        if (imagePlace == null) {
+        imagePlace.src = "images/bulb-off.png";
+        imagePlace.alt = "Becul este stins";
+        imagePlace.title = "Becul este stins";
+        imagePlace != null;
+    }
+}
+}
