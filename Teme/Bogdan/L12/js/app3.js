828db5d9ade95e831f619da820b7fd346a8708bc
BogdanG93 <bogdan_geagu93@yahoo.com>
2020-03-03 16:06:31 +0200

    problem solved

--- Teme/Bogdan/L12/js/app3.js
+++ Teme/Bogdan/L12/js/app3.js
@@ -1,22 +1,21 @@
 var imagePlace;
+var x=1;
 
 function Aprins() {
     imagePlace = document.getElementById('Light-Bulb');
 
-    if (imagePlace != null) {
+    if (x==1) {
         imagePlace.src = "images/bulb-on.png";
         imagePlace.alt = "Becul este aprins";
         imagePlace.title = "Becul este aprins";
-        imagePlace == null;
+        x--;
     }
-function Stins() {
-    imagePlace = document.getElementById('Light-Bulb');
-    
-    if (imagePlace == null) {
-        imagePlace.src = "images/bulb-on.png";
-        imagePlace.alt = "Becul este aprins";
-        imagePlace.title = "Becul este aprins";
-        imagePlace != null;
+    else {
+        if (x==0) {
+            imagePlace.src = "images/bulb-off.png";
+            imagePlace.alt = "Becul este stins";
+            imagePlace.title = "Becul este stins";
+            x++;
     }
 }
 }
