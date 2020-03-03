32da422b009767d75748de8c3e6eaef9201a060c
Letisss <letitia.ionescu@yahoo.com>
2020-03-03 08:25:58 +0200

    tema 12 letitia ionescu

---
+++ Teme/LetiI/T12-13 Javascript/JS/app.js
@@ -0,0 +1,21 @@
+
+
+function ChangeColor1() {
+    var a;
+    a = document.getElementById("cos-cumparaturi");
+    if (a != null) {
+        a.style = "color: green";
+    }
+}
+function ChangeColor2() {
+    var a;
+    a = document.getElementById("cos-cumparaturi");
+    if (a != null) {
+        a.style = "color: pink";
+    }
+}
+function Sum() {
+    var b;
+    b = 35 + 41;
+    document.getElementById("adunare").innerText = b;
+}
