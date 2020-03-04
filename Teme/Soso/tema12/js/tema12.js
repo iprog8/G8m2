5d083c2514cc1cbb51f78f0c9f29ec6c0a78bfff
Your Name <you@example.com>
2020-03-04 23:01:24 +0200

    corecturi tema 12+tema13 partial

--- Teme/Soso/tema12/js/tema12.js
+++ Teme/Soso/tema12/js/tema12.js
@@ -1,20 +1,22 @@
-function violet() {
+function ChangeColorToViolet() {
     var a;
     a = document.getElementById("cos-cumparaturi");
     if (a != null) {
-        a.style = "color: Violet";
+        a.classList.add("violet-color");
+        a.classList.remove("dogerblue-color");
     }
 }
-function albastru() {
+function ChangeColorToBlue() {
     var a;
     a = document.getElementById("cos-cumparaturi");
     if (a != null) {
-        a.style = "color: DodgerBlue";
+        a.classList.add("dogerblue-color");
+        a.classList.remove("violet-color");
     }
 }
 
 function adunare() {
-    var s;
-    s = 35 + 41;
-    document.getElementById("suma").innerText = s;
+    var sum;
+    sum = 35 + 41;
+    document.getElementById("suma").innerText = sum;
 }
