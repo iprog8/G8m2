2fc8457a7949036327feb1a09adea8305c80ccd8
Alina Stefania Rudi <alina.rudi@uipath.com>
2020-03-05 12:04:08 +0200

    corectare tema 12 + tema 13

--- Teme/AlinaR/L12/js/app1.js
+++ Teme/AlinaR/L12/js/app1.js
@@ -2,28 +2,16 @@
 function Turn() {
     imagePlace = document.getElementById("lightImg")
     if (imagePlace != null) {
-        if (imagePlace.src == "file:///C:/Users/alina.rudi/Desktop/G8m2-1/Teme/AlinaR/L12/images/bulboff.png") {
-            imagePlace.src = "file:///C:/Users/alina.rudi/Desktop/G8m2-1/Teme/AlinaR/L12/images/bulbon.png";
+        if (imagePlace.src.includes("images/bulbon.png")) {
+            imagePlace.src = "images/bulboff.png";
+            imagePlace.alt = "Becul este stins";
+            imagePlace.title = "Becul este stins";
+        }
+        else {
+            imagePlace.src = "images/bulbon.png";
             imagePlace.alt = "Becul este aprins";
             imagePlace.title = "Becul este aprins";
         }
-        else {
-            if (imagePlace.src == "file:///C:/Users/alina.rudi/Desktop/G8m2-1/Teme/AlinaR/L12/images/bulbon.png") {
-                imagePlace.src = "file:///C:/Users/alina.rudi/Desktop/G8m2-1/Teme/AlinaR/L12/images/bulboff.png";
-                imagePlace.alt = "Becul este stins";
-                imagePlace.title = "Becul este stins";
-            }
-            else {
-                imagePlace.src = "file:///C:/Users/alina.rudi/Desktop/G8m2-1/Teme/AlinaR/L12/images/bulbon.png";
-                imagePlace.alt = "Becul este aprins";
-                imagePlace.title = "Becul este aprins";
-            }
-
-        }
-
-    }
-    else {
-        console.warn("nu am gasit unde sa pun becul")
     }
 }
 
