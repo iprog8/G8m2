b2b4b17ef66646a9664482855616b72d9aa850b0
BogdanG93 <bogdan_geagu93@yahoo.com>
2020-03-09 12:13:36 +0200

    tema dezvoltata L13

--- Teme/Bogdan/L13/js/app2.js
+++ Teme/Bogdan/L13/js/app2.js
@@ -36,3 +36,42 @@ function Greybkgd() {
     document.getElementById("cos-cumparaturi").classList.remove("lemon-bkgd");
     document.getElementById("cos-cumparaturi").classList.remove("lime-bkgd");
 }
+
+function AfiseazaData() {
+    var backgroundcolor = document.getElementById("cos-cumparaturi");
+    var lemon=0;
+    var lime=0;
+    var grey=0;
+    for (let index = 1; index <= 10; index++) {
+        var date = new Date();
+        var day = ("0" + date.getDate()).slice(-2);
+        var month = ("0" + (date.getMonth() + 1)).slice(-2);
+        var year = date.getFullYear();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+        if (backgroundcolor != null) {
+            if (seconds % 3 == 0) {
+                backgroundcolor.classList.add("lemon-bkgd");
+                backgroundcolor.classList.remove("grey-bkgd");
+                backgroundcolor.classList.remove("lime-bkgd");
+                lemon++;
+            }
+            else if (seconds % 3 == 1) {
+                backgroundcolor.classList.add("lime-bkgd");
+                backgroundcolor.classList.remove("grey-bkgd");
+                backgroundcolor.classList.remove("lemon-bkgd");
+                lime++;
+            }
+            else {
+                backgroundcolor.classList.add("grey-bkgd");
+                backgroundcolor.classList.remove("lime-bkgd");
+                backgroundcolor.classList.remove("lemon-bkgd");
+                grey++;
+            }
+        } 
+        alert( index + "." + "Data curenta este:" + day + "." + month + "." + year + "   " + "Ora :" + hours + ":" + minutes +"'" + seconds);
+    }
+    alert("Culoarea background-ului s-a schimbat in felul urmator:\n" + "Lemon-" + lemon + "\n" + "Lime-" + lime + "\n" + "Grey-" + grey + ".");
+}
+  
