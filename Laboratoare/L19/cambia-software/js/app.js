46c6c1ed0167c02db007e8c077d012c530b9413d
Ovidiu Rudi <djvidov@yahoo.com>
2020-04-12 13:15:34 +0300

    lab 19

--- Laboratoare/L19/cambia-software/js/app.js
+++ Laboratoare/L19/cambia-software/js/app.js
@@ -5,7 +5,8 @@ var appHeader = new Vue({
         name1: 'Cambia',
         name2: 'Software',
         home: 'Cambia software',
-        about: 'Despre noi',
+        howWorks: 'Cum lucram?',
+        mobileReady: 'Mobile ready',
         features: 'Beneficii',
         screenshots: 'Poze aplicatie',
         screenshotsVisibility: true,
@@ -34,18 +35,68 @@ var appLanding = new Vue({
     el: '#landingArea',
     data: {
         welcome: 'Bine ai venit la Cambia Software',
-        motto: 'Aplicatiile noastre iti Cambia business-ul in bine',
-        readMore: 'Afla mai multe despre Cambia Software'
+        motto: 'Aplicatiile noastre iti schimba business-ul in bine',
+        readMore: 'Cum lucram?'
     },
     methods: {
     }
 });
 
-var appLanding = new Vue({
-    el: '#landingArea',
+var appGetStarted = new Vue({
+    el: '#get-started',
     data: {
-
+        title: 'Implementare in pasi simpli',
+        subtitle: 'Pasii nu sunt batuti in cuie dar am observat ca acestia functioneaza',
+        steps: [
+            {
+                image: 'img/svg/cloud.svg',
+                title: '1. Dorinte',
+                text: 'Tu ne spui ce iti doresti de la site si noi iti dam o estimare la ceea ce iti doresti',
+                readMore: 'Detalii',
+                readMoreLink: 'dorinte.html',
+                addRow: 0
+            },
+            {
+                image: 'img/svg/planet.svg',
+                title: '2. Identitatea ta',
+                text: 'Vii cu identitatea ta (textele pentru site) dar care poate include poze, culori si logo',
+                readMore: 'Detalii',
+                readMoreLink: 'identitatea-ta.html',
+                addRow: 0
+            },
+            {
+                image: 'img/svg/paint-palette.svg',
+                title: '3. Aspect site',
+                text: 'Alegi layout-ul si paleta de culori din cele 3 propuse de noi \n',
+                readMore: 'Detalii',
+                readMoreLink: 'aspect-site.html',
+                addRow: 1
+            },
+            {
+                image: 'img/svg/design-tool.svg',
+                title: '4. Finisaje',
+                text: 'Dupa ce iti livram site-ul facem mici ajustari pentru a-l face sa arate asa cum iti doresti',
+                readMore: 'Detalii',
+                readMoreLink: 'finisaje.html',
+                addRow: 0
+            },
+        ]
     },
     methods: {
     }
 });
+
+var appMobileReady = new Vue({
+    el: '#mobile-ready',
+    data: {
+        title1: 'Site-urile',
+        title2: 'Cambia Software',
+        title3: 'sunt mobile ready',
+        description: `Pentru ca in ziua de azi peste 50% din vizitatorii unui site vin de pe telefoane mobile 
+        este impetuos necesar ca orice site creat sa se vada bine pe aceste device-uri si sa aiba scurtaturile necesare 
+        pentru a putea fi usor de contactat via Email, Telefon si WhatsApp`
+    },
+    methods: {
+
+    }
+})
